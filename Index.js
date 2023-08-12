dfe807397fd7fae9794dbb6f7e0c58bf93dd6192
Tharaneswar09 <127746343+Tharaneswar09@users.noreply.github.com>
2023-08-12 15:20:23 +0530

    Update and rename Index.js to sw.js

--- Index.js
+++
@@ -1,1 +0,0 @@
-<script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://inklinkor.com/tag.min.js',6213235,document.body||document.documentElement)</script>
